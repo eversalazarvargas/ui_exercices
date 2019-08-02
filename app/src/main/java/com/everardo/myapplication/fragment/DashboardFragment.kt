f2da244cb91786bb6b2ace1235564743155d8cf6
everardo <everardo.salazar@disney.com>
2019-08-02 00:26:56 -0700

    add more

---
+++ app/src/main/java/com/everardo/myapplication/fragment/DashboardFragment.kt
@@ -0,0 +1,11 @@
+package com.everardo.myapplication.fragment
+
+import android.support.v4.app.Fragment
+
+
+/**
+ * @author everardo.salazar on 8/2/19
+ */
+class DashboardFragment: Fragment() {
+
+}
